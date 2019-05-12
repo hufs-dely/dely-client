15dbc063e21003f8a270d5032c8d1c541fd65aa0
kminny <darkdevil94@naver.com>
2019-05-13 07:02:12 +0900

    .gitignore test

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,6 +1,6 @@
 import { GoogleApiWrapper } from "google-maps-react";
-// import { MAPS_KEY } from "../../keys";
+import { MAPS_KEY } from "../../keys";
 import HomeContainer from "./HomeContainer";
 export default GoogleApiWrapper({
-  apiKey: process.env.MAPS_KEY
+  apiKey: MAPS_KEY
 })(HomeContainer);
